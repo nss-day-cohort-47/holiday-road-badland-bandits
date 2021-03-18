0c9db6edd16fc0d273bdb95625760b0523fe6c0e
BenjaminGolden <b.j.golden@outlook.com>
2021-03-18 16:01:22 -0500

    details display working

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 // import { settings } from "./Settings.js"
-import {parkList, parkPreviewList} from "./parks/ParkList.js"
+import {parkList, parkPreviewList, parkPreviewDetails} from "./parks/ParkList.js"
 import {loadParks, useParks} from "./parks/ParkProvider.js"
 import {loadWeather, useWeather} from "./weather/WeatherProvider.js"
 import {weatherCard} from "./weather/WeatherCard.js"
@@ -11,13 +11,10 @@ import {attractionList} from "./attractions/AttractionList.js"
 
 const displayPark = (parkId) => {
     console.log(parkId, useParks() )
-    const displayArray = []
-    useParks().find((singlePark) => {
-        if (singlePark.id === (parkId)){
-            displayArray.push(singlePark);
-        }
+    const selectedPark = useParks().find(singlePark => {
+        return singlePark.id === (parkId)
     })
-    return displayArray;
+    return selectedPark
 }
 
 
@@ -27,13 +24,6 @@ navElement.addEventListener("change", event => {
         const singlePark = displayPark(event.target.value)
         console.log("show me this national park")
 		parkPreviewList(singlePark)
-        loadWeather(singlePark.map(weatherObj =>{
-            return weatherObj.addresses[0].city;
-        })).then(weather =>{
-            weatherCard(weather);
-            console.log(weather);
-            
-        });
     } 
     
 })
@@ -41,10 +31,31 @@ navElement.addEventListener("change", event => {
 
 const previewElement = document.querySelector("#park_preview")
 previewElement.addEventListener("click", event => {
-    if(event.target.id === "container_details--park"){
-        console.log("park details")
+    console.log("btn clicked ", event)
+    if(event.target.id.startsWith("container_details--park")) {
+        const parkId = event.target.id.split("__")[1]
+        console.log("park id", parkId);
+        const singleParkDetails = displayPark(parkId)
+        console.log(singleParkDetails)
+        parkPreviewDetails(singleParkDetails)
     }
 })
+
+
+
+
+
+
+//i need to make a function that finds the details of the selected park
+// const parkDetails =  (showParkDetails) => {
+//     const parkDetailsArray = []
+//     useParks().filter(parkPreviewDetails => {
+//         if (parkPreviewDetails.data.includes(showParkDetails)){
+//             return parkDetails;
+//         }
+//     })
+//     parkList(parkDetailsArray);
+// }
 
 loadParks().then(() => {
     const foundParks = useParks();
--- scripts/parks/Park.js
+++ scripts/parks/Park.js
@@ -1,7 +1,20 @@
-export const park = (parkobj) => {
-    return `<section class = "park_preview">
-    <h3>${parkobj.fullName}</h3>
+export const park = (parkObj) => {
+    return `<section class = "park_preview">You Selected This National Park:
+    <h3>${parkObj.fullName}</h3>
     <div id="weather"></div>
-    <button id="container_details--park">Park Details:</button>
+    <button id="container_details--park__${parkObj.id}">Park Details:</button>
     </section>`
 }
+
+export const parkDetailsHTML = (parkDetailsObj) => {
+    return `<section id="park_preview--details">
+    <h3>Park Name: ${parkDetailsObj.fullName}</h3>
+    <div>Description: ${parkDetailsObj.description}</div>
+    <div>Url: ${parkDetailsObj.url}</div>
+    <div>Address: ${parkDetailsObj.addresses}</div>
+    <div>Weather: ${parkDetailsObj.weatherInfo}</div>
+    </section>`
+};
+
+
+//class="isHidden"-css- hide and show elements with css display none using classes
