72c9b78be18f614f0781daa47b4863c288993f5f
micaelacolpi <micaelacolpi@yahoo.com>
2021-03-18 16:01:28 -0600

    Title of bizarre attraction is now showing on the dom

--- scripts/attractions/AttractionList.js
+++ scripts/attractions/AttractionList.js
@@ -1,6 +1,6 @@
 
 import { attractionDropdown } from "./AttractionDropdown.js";
-
+import {attraction} from "./Attraction.js"
 
 
 
@@ -12,3 +12,13 @@ export const attractionList = (list) => {
         attractionDisplay.innerHTML = attractionHTML;
          }
 
+
+export const attractionName = (list) => { 
+    // console.log(list)
+let attractionHTML = ""; 
+const attractionDisplay = document.querySelector(".bizarreries_preview");
+for (const attractionobj of list) { 
+attractionHTML += attraction(attractionobj)
+console.log(attractionHTML) }
+attractionDisplay.innerHTML = attractionHTML;
+}
